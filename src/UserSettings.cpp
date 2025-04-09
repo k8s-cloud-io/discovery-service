51da48271abd641a92c1c5fe0a21bec99aede625
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-04-09 16:17:26 +0200

    UserSettings: check file permissions

--- src/UserSettings.cpp
+++ src/UserSettings.cpp
@@ -7,15 +7,22 @@
 UserSettings::UserSettings()
 :settingsFile("/etc/discovery-service/default.json") {
   User user = User::current();
-  String filePath = user.getHomeDirectory().append("/.config/discovery-service/settings.json");
-  if(File::exists(filePath)) {
-    settingsFile = filePath;
+  String localSettingsFile = user.getHomeDirectory().append("/.config/discovery-service/settings.json");
+  if(File::exists(localSettingsFile)) {
+    settingsFile = localSettingsFile;
   }
 
   if(!File::exists(settingsFile)) {
     std::cout << "unable to read settings file '" << settingsFile << "': file does not exist" << std::endl;
     exit(1);
   }
+
+  if(!File::isReadable(settingsFile)) {
+    std::cout << "unable to read settings file '" << settingsFile << "': file is not readable" << std::endl;
+    exit(1);
+  }
+
+  String content = File(settingsFile).getBytes();
 }
 
 UserSettings::~UserSettings() {
