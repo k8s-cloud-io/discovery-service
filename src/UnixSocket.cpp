5068927e8c20f8d1cb616cc76094dd717387012b
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-04-14 16:40:18 +0200

    remove binding from socket

--- src/UnixSocket.cpp
+++ src/UnixSocket.cpp
@@ -1,32 +1,12 @@
 #include "UnixSocket.h"
 
-#include <csignal>
 #include <Dir.h>
 #include <File.h>
 #include <iostream>
-#include <list>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <thread>
-
-typedef struct _Binding {
-	int fd;
-	const char *path;
-} Binding;
-typedef std::list<Binding> BindingList;
-static BindingList bindings;
-
-void removeBinding(const Binding binding) {
-	for (auto it = bindings.begin(); it != bindings.end(); ++it) {
-		if (it->fd == binding.fd) {
-			::close(it->fd);
-			unlink(binding.path);
-			bindings.erase(it);
-			break;
-		}
-	}
-}
 
 UnixSocket::UnixSocket(const String &path)
 	:listening(false), path(path), fd(-1) {
@@ -53,10 +33,6 @@ void UnixSocket::listen() {
 	address.sun_family = AF_UNIX;
 	strcpy(address.sun_path, path.c_str());
 	fd = socket(AF_UNIX, SOCK_STREAM, 0);
-	const Binding socketBinding = {
-		.fd = fd,
-		.path = path.c_str(),
-	};
 
 	if (fd == -1) {
 		std::cout << "error in UnixSocket: unable to create socket" << std::endl;
@@ -65,24 +41,21 @@ void UnixSocket::listen() {
 
 	if (bind(fd, reinterpret_cast<sockaddr *>(&address), sizeof(address)) != 0) {
 		std::cout << "error in UnixSocket: unable to bind socket" << std::endl;
-		removeBinding(socketBinding);
 		exit(1);
 	}
 
 	if (::listen(fd, 100) != 0) {
 		std::cout << "error in UnixSocket: unable to listen on socket" << std::endl;
-		removeBinding(socketBinding);
 		exit(1);
 	}
 
 	listening = true;
-	runner = std::thread([this, socketBinding]() {
+	runner = std::thread([this]() {
 		while (isListening()) {
 			// TODO implement IO: without, the service is stopping
 			sleep(1);
 			std::cout << "Listening..." << std::endl;
 		}
-		removeBinding(socketBinding);
 	});
 }
 
