707bde35f20b324d882a83413e00c72d15a8d256
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-04-29 11:21:24 +0200

    FeedLoader: comment example code

--- src/FeedLoader.cpp
+++ src/FeedLoader.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
 #include <iostream>
+#include "DateTime.h"
 #include "FeedLoader.h"
 #include "FeedReader.h"
 #include "Exception.h"
 #include "SqlDatabase.h"
 #include "SqlQuery.h"
-#include "SqlRecord.h"
 #include "Sqlite3Driver.h"
 #include "User.h"
 
@@ -33,39 +33,41 @@ void FeedLoader::load(const StringList &list) {
         db.setDriver(driver);
 
         // create table if not exists
-        SqlQuery createTable("CREATE TABLE IF NOT EXISTS feed_item (id INTEGER PRIMARY KEY, feed_url VARCHAR(255), link VARCHAR(255), pub_date VARCHAR(255), title VARCHAR(255))", db);
+        SqlQuery createTable("CREATE TABLE IF NOT EXISTS feed_item (id INTEGER PRIMARY KEY, feed_url VARCHAR(255), link VARCHAR(255), pub_date VARCHAR(255), title VARCHAR(255))", nullptr, db);
         createTable.exec();
 
         for(String feed: list) {
             std::cout << "load feed " << feed << std::endl;
             FeedList feedItems = FeedReader::loadFeed(feed);
             
-            SqlQuery removeQuery("DELETE FROM feed_item WHERE feed_url = :1", db);
+            // delete old feed items from table
+            SqlQuery removeQuery("DELETE FROM feed_item WHERE feed_url = :1", "feed_item", db);
             removeQuery.bindValue(1, feed);
             removeQuery.exec();
 
+            // insert feed items into table
             for(FeedItem item: feedItems) {
-                SqlQuery insertQuery("INSERT INTO feed_item (feed_url, link, pub_date, title) VALUES(:1, :2, :3, :4)", db);
+                SqlQuery insertQuery("INSERT INTO feed_item (feed_url, link, pub_date, title) VALUES(:1, :2, :3, :4)", "feed_item", db);
                 insertQuery.bindValue(1, feed);
                 insertQuery.bindValue(2, item.link);
                 insertQuery.bindValue(3, item.pubDate);
                 insertQuery.bindValue(4, item.title);
                 insertQuery.exec();
-
-                std::cout << item.title << std::endl;
             }
 
-            SqlQuery selection("SELECT * FROM feed_item WHERE feed_url = :1", db);
+            std::cout << DateTime::currentDateTime().toString() << ": feed was loaded successfully" << std::endl;
+
+            /*
+            SqlQuery selection("SELECT pub_date FROM feed_item WHERE feed_url = :1", "feed_item", db);
             selection.bindValue(1, feed);
             selection.exec();
+
             auto result = selection.getResult();
             for(SqlRecord record: result->getRecords()) {
                 std::cout << record.getField(0).getValue().toString() << std::endl;
             }
-            std::cout << "got feed items: raw = " << feedItems.size() << ", inserted = " << result->getRecords().size() << std::endl;
             delete result;
-
-            // TODO insert feed items into database
+            */
         }
     } catch(const Exception &e) {
         std::cout << e.what() << " in class 'FeedLoader::load' - " << e.getMessage() << std::endl;
