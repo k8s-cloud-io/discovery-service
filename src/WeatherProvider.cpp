bbf3884997adc58f5bf56ad4852d17c244667b95
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-05-04 23:00:38 +0200

    WeatherProvider: support location

--- src/WeatherProvider.cpp
+++ src/WeatherProvider.cpp
@@ -1,10 +1,15 @@
 #include "WeatherProvider.h"
 #include "Credentials.h"
-
-WeatherProvider::WeatherProvider()
-:credentials(Credentials::CREDENTIALS_TYPE_NONE) {}
-
+#include "Location.h"
 
 void WeatherProvider::setCredentials(const Credentials &c) {
 	credentials = c;
+}
+
+void WeatherProvider::setLocation(const Location &loc) {
+	location = loc;
+}
+
+Location WeatherProvider::getLocation() const {
+	return location;
 }
--- src/include/WeatherProvider.h
+++ src/include/WeatherProvider.h
@@ -2,18 +2,23 @@
 #define WEATHER_PROVIDER_MANAGER_H
 
 #include "Credentials.h"
+#include "Location.h"
 
 class WeatherProvider {
 	public:
 
-		WeatherProvider();
+		WeatherProvider() = default;
 		virtual ~WeatherProvider() = default;
 
 		void setCredentials(const Credentials &);
+		void setLocation(const Location &);
 		virtual void load() const = 0;
+
+		Location getLocation() const;
 
 	protected:
 		Credentials credentials;
+		Location location;
 };
 
 #endif // WEATHER_PROVIDER_MANAGER_H
