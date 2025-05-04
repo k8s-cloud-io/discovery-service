0524e19559284746c602c98321492ecee56a3eaa
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-05-04 23:01:31 +0200

    WorldWeatherOnlineProvider: start implement requestto endpoint

--- src/weather/WorldWeatherOnlineProvider.cpp
+++ src/weather/WorldWeatherOnlineProvider.cpp
@@ -1,6 +1,19 @@
 #include "weather/WorldWeatherOnlineProvider.h"
+#include "Exception.h"
+#include "HttpRequest.h"
 #include "Logger.h"
 
+#define END_POINT "https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json"
+
 void WorldWeatherOnlineProvider::load() const {
-    Logger::log("WorldWeatherOnlineProvider is loading");
+    Logger::log("WorldWeatherOnlineProvider is loading...");
+    if(credentials.getApiKey().empty()) {
+        throw Exception("Exception in class WorldWeatherOnlineProvider: unable to load weather data, missing api key.");
+    }
+
+    HttpRequest request(HttpRequest::GET, END_POINT);
+    HttpResponse *response = request.exec();
+    String body = response->getBody();
+
+    Logger::log(body);
 }
