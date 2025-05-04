429bad966851c26c7b7ed3a73d75640655cc3eb6
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-05-04 15:54:17 +0200

    remove logging

--- src/CinnamonDiscovery.cpp
+++ src/CinnamonDiscovery.cpp
@@ -1,6 +1,5 @@
 #include "CinnamonDiscovery.h"
 #include "FeedLoader.h"
-#include "Logger.h"
 #include "Timer.h"
 #include "UserSettings.h"
 #include "WeatherProvider.h"
@@ -12,14 +11,12 @@ void CinnamonDiscovery::init() {
     Timer t;
     t.setInterval(1000 * 30);
     t.start([settings](Timer *) {
-        Logger::log("Start Feed Loader");
         FeedLoader::load(settings.getFeeds());
     });
 
     Timer t2;
     t2.setInterval(1000 * 30);
     t2.start([this, provider](Timer *){
-         Logger::log("Start Weather Provider");
          provider->load();
     });
 }
