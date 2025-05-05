006b45c755ea99b4aaf20f5a40615ebf55809b99
Andreas Bratfisch <andreas.bratfisch@seitwerk.de>
2025-05-05 12:13:09 +0200

    WorlWeatherOnline: support real url in request

--- src/weather/WorldWeatherOnlineProvider.cpp
+++ src/weather/WorldWeatherOnlineProvider.cpp
@@ -3,7 +3,7 @@
 #include "HttpRequest.h"
 #include "Logger.h"
 
-#define END_POINT "https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json"
+#define END_POINT "https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json&fx=yes&fx24=yes&num_of_days=5&date_format=unix&key=%1&q=%2"
 
 void WorldWeatherOnlineProvider::load() const {
     Logger::log("WorldWeatherOnlineProvider is loading...");
@@ -11,7 +11,13 @@ void WorldWeatherOnlineProvider::load() const {
         throw Exception("Exception in class WorldWeatherOnlineProvider: unable to load weather data, missing api key.");
     }
 
-    HttpRequest request(HttpRequest::GET, END_POINT);
+    String s(END_POINT);
+    s.arg(2, credentials.getApiKey().c_str(), "Miesbach");
+
+    Logger::log(s);
+    Url u = Url::parse(s);
+
+    HttpRequest request(HttpRequest::GET, u);
     HttpResponse *response = request.exec();
     String body = response->getBody();
 
